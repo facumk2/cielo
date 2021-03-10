6cf7204fca7223437c9ddcf0bfdbfe1826fd535d
Facundo Mercado <facundo_merk2@hotmail.com>
2021-03-09 23:56:23 -0300

    wip: ocultando botones

--- src/views/word/index.js
+++ src/views/word/index.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import "./index.css";
 
 const Word = () => {
+  const [state, setState] = useState({
+    button1: true,
+    button2: false,
+    button3: false,
+    button4: false,
+    button5: false,
+    button6: false,
+  });
+
+  const handleClick = (event) => {
+    const id = parseInt(event.target.id);
+    console.log(id);
+    console.log(state);
+    const siguienteBoton = `button${id + 1}`;
+    const actualBoton = `button${id}`;
+    setState((prev) => ({
+      ...prev,
+      [siguienteBoton]: true,
+      [actualBoton]: false,
+    }));
+    console.log(siguienteBoton);
+  };
+
   return (
     <div>
       <main>
@@ -11,12 +35,36 @@ const Word = () => {
 
         <div className="config-conteiner">
           <div className="action-conteiner">
-            <button>Primer Boton</button>
-            <button>Segundo Boton</button>
-            <button>Tercero Boton</button>
-            <button>Cuarto Boton</button>
-            <button>Quinto Boton</button>
-            <button>Sexto Boton</button>
+            {state.button1 ? (
+              <button id="1" onClick={handleClick}>
+                Primer Boton
+              </button>
+            ) : null}
+            {state.button2 ? (
+              <button id="2" onClick={handleClick}>
+                Segundo Boton
+              </button>
+            ) : null}
+            {state.button3 ? (
+              <button id="3" onClick={handleClick}>
+                Tercero Boton
+              </button>
+            ) : null}
+            {state.button4 ? (
+              <button id="4" onClick={handleClick}>
+                Cuarto Boton
+              </button>
+            ) : null}
+            {state.button5 ? (
+              <button id="5" onClick={handleClick}>
+                Quinto Boton
+              </button>
+            ) : null}
+            {state.button6 ? (
+              <button id="6" onClick={handleClick}>
+                Sexto Boton
+              </button>
+            ) : null}
           </div>
           <div className="bottom-menu">
             <nav className="bottom-menu__plants">
